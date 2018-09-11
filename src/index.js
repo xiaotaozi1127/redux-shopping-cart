7e98cefedeee9aecb585b9ea728b6d91eaf9354c
htao <htao@thoughtworks.com>
2018-09-11 09:10:00 +0800

    [taohui] manage shopping cart state with combined reducers

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { createStore, combineReducers } from "redux";
+
+const initialState = {
+  cart: [
+    {
+      product: 'bread 700g',
+      quantity: 2,
+      unitCost: 90
+    },
+    {
+      product: 'milk 500ml',
+      quantity: 1,
+      unitCost: 47
+    }
+  ]
+}
+
+const productsReducer = function(state = initialState, action) {
+  return state;
+}
+
+const cartReducer = function(state = initialState, action) {
+  return state;
+}
+
+const allReducers = {
+	products: productsReducer,
+	shoppingCart: cartReducer
+}
+
+const rootReducer = combineReducers(allReducers);
+
+
+const store = createStore(rootReducer);
+console.log("initial state: ", store.getState());
