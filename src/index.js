a804cce69b3b07e231f083e3c3e29da79701d966
htao <htao@thoughtworks.com>
2018-09-11 13:50:32 +0800

    [taohui] spilt into different files

--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
-import { createStore, combineReducers } from "redux";
+import store from './store.js';
+import { addToCart } from './actions/cart-actions';
 
-const initialState = {
-  cart: [
-    {
-      product: 'bread 700g',
-      quantity: 2,
-      unitCost: 90
-    },
-    {
-      product: 'milk 500ml',
-      quantity: 1,
-      unitCost: 47
-    }
-  ]
-}
-
-const ADD_TO_CART = 'ADD_TO_CART';
-
-
-const productsReducer = function(state = [], action) {
-  return state;
-}
-
-const cartReducer = function(state = initialState, action) {
-	switch(action.type){
-		case ADD_TO_CART: {
-			return {
-				...state,
-				cart: [...state.cart, action.payload]
-			}
-		}
-		default:
-			return state;
-	}
-}
-
-function addToCart(product, quantity, unitCost) {
-  return {
-    type: ADD_TO_CART,
-    payload: { product, quantity, unitCost }
-  }
-}
-
-const allReducers = {
-	products: productsReducer,
-	shoppingCart: cartReducer
-}
-
-const rootReducer = combineReducers(allReducers);
-
-const store = createStore(rootReducer);
 console.log("initial state: ", store.getState());
 
 let unsubscribe = store.subscribe(() =>
